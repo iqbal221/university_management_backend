4d53650b4ec07a4f5e7a2d3835b4c85801b98b48
zahed iqbal <zahediiuc221@gmail.com>
2023-06-05 22:08:45 +0600

    setup eslint prettier husky

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function main() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log(`Database connected successfully`)
+  try {
+    await mongoose.connect(config.database_url)
+    console.log(`Database connected successfully`)
 
-        app.listen(config.port, () => {
-            console.log(`Application is running on port ${config.port}`)
-          })
-
-    } catch (error) {
-        console.log('Fail to connect to Database')
-    }
+    app.listen(config.port, () => {
+      console.log(`Application is running on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log('Fail to connect to Database')
+  }
 }
 
 main()
