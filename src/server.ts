001442f015269b8102da1ab23609dacdf609d9ff
zahed iqbal <zahediiuc221@gmail.com>
2023-06-05 15:24:42 +0600

    setup cors mongoose express

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function main() {
+    try {
+        await mongoose.connect(config.database_url as string);
+        console.log(`Database connected successfully`)
+
+        app.listen(config.port, () => {
+            console.log(`Application is running on port ${config.port}`)
+          })
+
+    } catch (error) {
+        console.log('Fail to connect to Database')
+    }
+}
+
+main()
